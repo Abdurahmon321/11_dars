3d4e26d3d2be482ea28d20332b40a08c254efaf0
Abdurahmon <abdurahmonhoshimov2004@gmail.com>
2024-03-16 23:43:03 +0500

    11-dars

---
+++ colours.py
@@ -0,0 +1,19 @@
+def colored_print(text, color="red"):
+    colors = {
+        "red": "\033[91m",
+        "green": "\033[92m",
+        "yellow": "\033[93m",
+        "blue": "\033[94m",
+        "magenta": "\033[95m",
+        "cyan": "\033[96m"
+    }
+    end_color = "\033[0m"
+
+    color_code = colors.get(color.lower(), "")
+    if not color_code:
+        return
+    return color_code + text + end_color
+
+
+def free_space():
+    print(colored_print("-" * 50, color="red"))
